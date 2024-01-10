2748f50253dc1e811476dd3cd604ee1254a2754c
Aleksandar Pavlov <a.pavvlov@gmail.com>
2024-01-11 01:08:12 +0200

    Integrated Swagger
    + Added dependancy for Swagger
    + Modified StyleVerseApplication.java
    
    Co-Authored-By: AleksYonchev <150821817+aleksyonchev@users.noreply.github.com>

--- styleverse/src/main/java/com/styleverse/StyleVerseApplication.java
+++ styleverse/src/main/java/com/styleverse/StyleVerseApplication.java
@@ -1,14 +1,13 @@
 package com.styleverse;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+@EnableSwagger2
 public class StyleVerseApplication {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        SpringApplication.run(StyleVerseApplication.class, args);
     }
 }
