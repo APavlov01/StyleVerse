90df23aaee03120100c29bcb10d3693e4788f5ed
Aleksandar Pavlov <a.pavvlov@gmail.com>
2024-01-10 23:19:22 +0200

    Created OrderItem entity

---
+++ styleverse/src/main/java/com/styleverse/entity/OrderItem.java
@@ -0,0 +1,53 @@
+package com.styleverse.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "order_item")
+public class OrderItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    private int quantity;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
