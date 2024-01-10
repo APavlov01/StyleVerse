7423b485a1173500b1e1b66742680ccb5c7434d7
Aleksandar Pavlov <a.pavvlov@gmail.com>
2024-01-10 23:51:45 +0200

    Created ProductDTO

---
+++ styleverse/src/main/java/com/styleverse/dto/ProductDTO.java
@@ -0,0 +1,9 @@
+package com.styleverse.dto;
+
+public class ProductDTO {
+    private Long id;
+    private String name;
+    private String description;
+    private double price;
+    private Long categoryId;
+}
