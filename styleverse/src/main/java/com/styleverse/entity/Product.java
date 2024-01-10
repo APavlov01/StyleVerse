46c44504e6fbf8260faf70691638236eed693108
Aleksandar Pavlov <a.pavvlov@gmail.com>
2024-01-10 23:04:11 +0200

    Project structure
    + Product.java

---
+++ styleverse/src/main/java/com/styleverse/entity/Product.java
@@ -0,0 +1,20 @@
+package com.styleverse.entity;
+import javax.persistence.*;
+
+@Entity
+@Table(name = "product")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String description;
+    private double price;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+    // Getters and setters
+}
