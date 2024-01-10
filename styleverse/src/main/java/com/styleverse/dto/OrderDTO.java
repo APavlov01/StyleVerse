437676c795c6a99ad68d26320646f3f9a0a489ef
Aleksandar Pavlov <a.pavvlov@gmail.com>
2024-01-10 23:53:11 +0200

    Created OrderDTO

---
+++ styleverse/src/main/java/com/styleverse/dto/OrderDTO.java
@@ -0,0 +1,11 @@
+package com.styleverse.dto;
+
+import java.util.Date;
+
+public class OrderDTO {
+    private Long id;
+    private Date orderDate;
+    private double totalAmount;
+    private Long customerId;
+    
+}
