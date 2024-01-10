6cd2bcc62a4249d22ef9cd4a408c94d9edc84a87
Aleksandar Pavlov <a.pavvlov@gmail.com>
2024-01-10 23:10:16 +0200

    Category entity added
    + Modified Product entity

--- styleverse/src/main/java/com/styleverse/entity/Product.java
+++ styleverse/src/main/java/com/styleverse/entity/Product.java
@@ -1,4 +1,5 @@
 package com.styleverse.entity;
+
 import javax.persistence.*;
 
 @Entity
@@ -16,5 +17,43 @@ public class Product {
     @JoinColumn(name = "category_id")
     private Category category;
 
-    // Getters and setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
 }
